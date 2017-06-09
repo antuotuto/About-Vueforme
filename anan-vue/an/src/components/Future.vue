62aa1babc2f09244dc1593f48be2b7f495154a51
anan <675988829@qq.com>
2017-06-09 16:43:44 +0800

    两年来开放男

--- anan-vue/an/src/components/Future.vue
+++ anan-vue/an/src/components/Future.vue
@@ -2,28 +2,34 @@
   <div id="future">
 
 
+
+
+
+    <!-- 在以前我对前端知识有强烈欲望但一直进步很小的时候我很想有个老师或是一个网站来帮我解答疑惑 但是事实上很多网站都是以盈利为目的去写东西 学习一项技术
+      得看到无数无关紧要的广告 在我的网页中我会排出所以和该项技术无关的内容 有的仅仅是知识 我想从现在开始 去做一个初级的互联网传播者 这就是我 我叫 王安安 -->
+
   </div>
 </template>
 
 <script>
-import {ans} from '@/data'
 
-export default {
-  name: 'future',
-  data () {
-    return {
-
+  export default {
+    name: 'future',
+    data() {
+      return {
+      }
+    },
+    computed: {
     }
-  },
-}
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style  scoped lang="scss">
-  #future{
-    height:100%;
-    width:100%;
-    background: rgba(0,0,0,.8);
+<style scoped lang="scss">
+  #future {
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, .8);
+    color: #fff;
   }
 
 
